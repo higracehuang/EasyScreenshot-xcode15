0d49a4c6daf6788b81bf90aaf0a017b42e289ff9
higracehuang <higracehuang@gmail.com>
2024-01-17 14:03:00 -0600

    Initial Commit

---
+++ EasyScreenshot/EasyScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyScreenshotApp.swift
+//  EasyScreenshot
+//
+//  Created by Le Huang on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct EasyScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
