32fe7bd6429444e10a92b4358d9e5c0cf3d1bf21
higracehuang <higracehuang@gmail.com>
2024-01-17 14:40:16 -0600

    Create a Menu with multiple menu items

--- EasyScreenshot/AppDelegate.swift
+++ EasyScreenshot/AppDelegate.swift
@@ -15,6 +15,69 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     let mainMenu = NSMenu()
     
+    /// Creating menu item: area capture
+    let itemSelectArea = NSMenuItem(
+      title: "Select an area",
+      action: #selector(Self.actionSelectArea(_:)),
+      keyEquivalent: "")
+    itemSelectArea.image = NSImage(
+      systemSymbolName: "rectangle.dashed",
+      accessibilityDescription: nil
+    )
+    itemSelectArea.target = self
+    mainMenu.addItem(itemSelectArea)
+    
+    /// Creating menu item: entire screen capture
+    let itemCaptureEntireScreen = NSMenuItem(
+      title: "Screenshot the entire screen",
+      action: #selector(Self.actionCaptureEntireScreen(_:)),
+      keyEquivalent: "")
+    itemCaptureEntireScreen.image = NSImage(
+      systemSymbolName: "macwindow.on.rectangle",
+      accessibilityDescription: nil
+    )
+    itemCaptureEntireScreen.target = self
+    mainMenu.addItem(itemCaptureEntireScreen)
+    
+    /// Creating menu item: window capture
+    let itemCaptureWindow = NSMenuItem(
+      title: "Capture a window",
+      action: #selector(self.actionCaptureWindow(_:)),
+      keyEquivalent: "")
+    itemCaptureWindow.image = NSImage(
+      systemSymbolName: "macwindow",
+      accessibilityDescription: nil
+    )
+    itemCaptureWindow.target = self
+    mainMenu.addItem(itemCaptureWindow)
+    
+    /// Creating a divider
+    mainMenu.addItem(.separator())
+    
+    /// Creating menu item:  quit the app
+    let itemQuit = NSMenuItem(
+      title: "Quit EasyScreenshot",
+      action: #selector(self.actionExitApp(_:)),
+      keyEquivalent: "")
+    itemQuit.target = self
+    mainMenu.addItem(itemQuit)
+    
     statusBarItem?.menu = mainMenu
+  }
+  
+  @objc private func actionExitApp(_ sender: Any?) {
+    NSApp.terminate(self)
+  }
+  
+  @objc private func actionCaptureEntireScreen(_ sender: Any?) {
+    
+  }
+  
+  @objc private func actionSelectArea(_ sender: Any?) {
+    
+  }
+  
+  @objc private func actionCaptureWindow(_ sender: Any?) {
+    
   }
 }
